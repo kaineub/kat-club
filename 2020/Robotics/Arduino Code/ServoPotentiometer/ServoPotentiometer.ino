eb3e5d03b099a31155cd1d01ba7c8134a5d287c6
kaineub <kaineub@gmail.com>
2020-02-20 17:45:48 -0800

    Add comments

--- 2020/Robotics/Arduino Code/ServoPotentiometer/ServoPotentiometer.ino
+++ 2020/Robotics/Arduino Code/ServoPotentiometer/ServoPotentiometer.ino
@@ -2,21 +2,22 @@
 
 int potPin = A5;  // analog pin for the potentiometer
 int val;    // variable to read the value from potentiometer
-int servoPin = 9;
+int servoPin = 9; // pin that the servo is controlled from
 
-Servo myservo;
+Servo myservo; // Create an object that allows us to control the servomotor
 
+// Runs once when the Arduino first turns on
 void setup() {
-  pinMode(potPin,INPUT);
-  Serial.begin(9600);
-  myservo.attach(servoPin);
+  pinMode(potPin,INPUT); // Set the potentiometer pin as an input
+  Serial.begin(9600); // Open a connection with the serial monitor
+  myservo.attach(servoPin); // tell myservo which pin the servomotor is on
 }
 
 void loop() {
   val = analogRead(potPin);            // reads the value of the potentiometer (value between 0 and 1023)
   //Serial.println(val);                 // print it to the serial monitor
-  val = map(val, 0, 1023, 0, 180);
-  Serial.println(val);
-  myservo.write(val);
-  delay(15);
+  val = map(val, 0, 1023, 0, 180);     // change the value from the potentiometer into a value between 0 and 180 (the servo can only rotate 180 degrees)
+  Serial.println(val);                 // print that value to the serial monitor
+  myservo.write(val);                  // tell the servo to move to the angle
+  delay(15);                           // delay for 15 ms to allow the servo to move to that angle
 }
