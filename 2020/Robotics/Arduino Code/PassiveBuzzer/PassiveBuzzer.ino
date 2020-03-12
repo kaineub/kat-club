d369b3c844dd6c2865a757ed0d62535e32f35cae
kaineub <kaineub@gmail.com>
2020-03-12 13:07:51 -0700

    Fill in buzzer code

--- 2020/Robotics/Arduino Code/PassiveBuzzer/PassiveBuzzer.ino
+++ 2020/Robotics/Arduino Code/PassiveBuzzer/PassiveBuzzer.ino
@@ -1,24 +1,24 @@
-#include <_______>
+#include <pitches.h>
  
-// notes in the melody:
+// notes in the melody (C Major Scale:
 int melody[] = {
   NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-int duration = _________;  // Note will play for half a second
-int buzzerPin = ____________
+int duration = 500;  // Note will play for half a second
+int buzzerPin = 8;
 
 void setup() {
- pinMode(buzzerPin,_____);
+ pinMode(buzzerPin,OUTPUT);
 }
  
 void loop() {  
   for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(______, melody[thisNote], duration);
+    // Make the sound for 0.5 seconds
+    tone(buzzerPin, melody[thisNote], duration);
 
     // Delay for 1 second
-    delay(_____);
+    delay(1000);
   }
    
   // restart after two seconds 
-  delay(________);
+  delay(2000);
 }
